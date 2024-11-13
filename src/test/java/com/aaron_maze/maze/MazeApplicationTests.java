b7d71c9e2b8344afb95da030af2c4e57fd2425fe
AaronPuertollano <72194237+AaronPuertollano@users.noreply.github.com>
2024-11-13 15:55:38 +0100

    Inici

---
+++ src/test/java/com/aaron_maze/maze/MazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaron_maze.maze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
